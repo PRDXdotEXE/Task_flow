9b82bd1684b3bcfd7792d9b10aecead45b7de5a6
Pratik <pratikpaudel88@gmail.com>
2026-07-14 22:54:09 +0545

    new error fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,15 @@
-
 import Navbar from "../components/Navbar";
 import Dashboard from "../components/Dashboard";
-import Taskform from "../components/Taskform";
-import TaskList from "../components/TaskList";
-import Footer from "../components/Footer";
+
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <Dashboard />
-   
-    </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <Dashboard/>
+
+        </div>
+    );
 };
 
 export default Home;
